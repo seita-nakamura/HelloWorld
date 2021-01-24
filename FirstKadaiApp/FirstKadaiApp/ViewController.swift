4f9862adaf9d43550cc113da8215e30108d35a82
seita-nakamura <poke.basket@icloud.com>
2021-01-24 21:11:54 +0900

    はじめてのアプリのラベルを追加

---
+++ FirstKadaiApp/FirstKadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by sei on 2021/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
